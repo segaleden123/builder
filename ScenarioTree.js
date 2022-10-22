763848638561f57cbebab75f48285e92b62570a1
segaleden123 <79809843+segaleden123@users.noreply.github.com>
2022-10-22 17:56:16 +0300

    Add files via upload

---
+++ ScenarioTree.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Bluediv from '../styledComponents/blueDiv'
+
+const ScenarioTree = () => {
+
+    return <>
+    <div className='shadowBox' style={{height:'100%'}}> <Bluediv label="Scenario Tree"/> </div>
+       
+
+    </>
+}
+
+export default ScenarioTree
