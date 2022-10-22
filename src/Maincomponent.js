fc489c5a1e1d701fb141ad9bf0b8e2cd4a2bb9ea
segaleden123 <79809843+segaleden123@users.noreply.github.com>
2022-10-22 17:54:45 +0300

    Delete src directory

--- src/Maincomponent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './Maincomponent.css'
-const Maincomponent = () => {
-    return (
-        <div className="Maincomponent"><h2>dsfsd</h2></div>
-    )
-}
-
-export default Maincomponent;
--- src/assests/available-functions.js
+++
@@ -1,5 +0,0 @@
-export const functions = [
-    'Abs',
-    'Acos',
-    'Asin'
-]
